690c6b78016e500092fd4942ba3cb2e5716b6cd1
Mphasis <madhukar.jayapal@mphasis.com>
2017-11-30 09:59:06 -0800

    add README

---
+++ Mphasis_ISSP_Najmul/Cell/ISSPCell.swift
@@ -0,0 +1,9 @@
+//
+//  ISSPCell.swift
+//  Mphasis_ISSP_Najmul
+//
+//  Created by Najmul Hasan on 11/30/17.
+//  Copyright © 2017 Najmul Hasan. All rights reserved.
+//
+
+import Foundation
---
+++ Mphasis_ISSP_Najmul/Lib/DataOrganizer.swift
@@ -0,0 +1,36 @@
+//
+//  DataOrganizer.swift
+//  Mphasis_ISSP_Najmul
+//
+//  Created by Najmul Hasan on 11/30/17.
+//  Copyright © 2017 Najmul Hasan. All rights reserved.
+//
+
+import Foundation
+
+class DataOrganizer: NSObject {
+    
+   public static func formateDateFromTimeStamp(_ timeStamp: Double)-> String{
+        
+        let date = Date(timeIntervalSince1970: timeStamp)
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT") //Set timezone that we want
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "MMM dd, yyyy     HH:mm" //Specify format that is needed
+        let strDate = dateFormatter.string(from: date)
+        
+        return strDate
+    }
+    
+    public static func formateDuration(_ duration: Double)-> String{
+        
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.allowedUnits = [ .minute, .second ]  //If the duration is longer then add .hour first
+        formatter.zeroFormattingBehavior = [ .pad ]
+        
+        let formattedDuration = formatter.string(from: duration)
+        
+        return formattedDuration!
+    }
+}
